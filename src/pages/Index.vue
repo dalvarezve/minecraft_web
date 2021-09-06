c7a192546f0c050151481f73e55e35e1161bbf42
dalvarezve <wave10496@gmail.com>
2021-09-06 16:02:13 +0200

    first

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Minecraft Logo"
+      src="~assets/minecraftLogo.svg"
+      style="height: 45%"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
